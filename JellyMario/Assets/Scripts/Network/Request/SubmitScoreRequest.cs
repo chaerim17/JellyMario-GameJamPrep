e09db03e65ff0f5b45d3358ce66a90053287a8cc
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-07-23 17:37:57 +0900

    feat(client-framework): 웹 데이터 모델 구조 구현
    - Request 폴더 생성
    - Response 폴더 생성
    - SubmitScoreRequest 데이터 클래스 추가
    - RankingData 데이터 클래스 추가
    - RankingResponse 데이터 클래스 추가

---
+++ JellyMario/Assets/Scripts/Network/Request/SubmitScoreRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace JellyMario.Network.Request
+{
+    // 점수 등록 요청 데이터
+    [System.Serializable]
+    public class SubmitScoreRequest
+    {
+        // 플레이어 이름
+        public string playerName;
+
+        // 플레이어 점수
+        public int score;
+    }
+}
