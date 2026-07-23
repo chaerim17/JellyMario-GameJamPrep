6ebd10967e3565662963fc72099d016d00ea35fc
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-07-23 18:34:52 +0900

    feat(Player): 플레이어 프레임워크 구현
    - PlayerBase 클래스 추가
    - PlayerController 클래스 추가
    - PlayerManager 클래스 추가
    - 플레이어 공통 기능 구조 정의
    - 플레이어 등록 기능 추가

---
+++ JellyMario/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using JellyMario.Core;
+using JellyMario.Player;
+
+namespace JellyMario.Managers
+{
+    // 플레이어를 관리하는 매니저
+    public class PlayerManager : Singleton<PlayerManager>
+    {
+        // 현재 플레이어
+        private PlayerBase _player;
+
+        // 현재 플레이어 읽기
+        public PlayerBase Player => _player;
+
+        // 플레이어 등록
+        public void RegisterPlayer(PlayerBase player)
+        {
+            _player = player;
+        }
+
+        // 플레이어 제거
+        public void ClearPlayer()
+        {
+            _player = null;
+        }
+    }
+}
---
+++ JellyMario/Assets/Scripts/Player/PlayerBase.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace JellyMario.Player
+{
+    // 모든 플레이어의 부모 클래스
+    public class PlayerBase : MonoBehaviour
+    {
+        // 플레이어 초기화
+        protected virtual void Initialize()
+        {
+
+        }
+
+        // 이동
+        public virtual void Move()
+        {
+
+        }
+
+        // 달리기
+        public virtual void Run()
+        {
+
+        }
+
+        // 점프
+        public virtual void Jump()
+        {
+
+        }
+
+        // 피격
+        public virtual void Hit()
+        {
+
+        }
+
+        // 사망
+        public virtual void Die()
+        {
+
+        }
+
+        // 애니메이션 변경
+        protected virtual void SetAnimation(string animationName)
+        {
+
+        }
+
+        protected virtual void Awake()
+        {
+            Initialize();
+        }
+    }
+}
