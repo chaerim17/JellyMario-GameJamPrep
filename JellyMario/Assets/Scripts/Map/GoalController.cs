7f34b518dac67f1d78c7c737b7ca3151b4aa5a52
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-07-24 04:42:18 +0900

    feat(client-framework): 맵 프레임워크 구현
    - GoalController 추가
    - DeathZone 추가
    - InteractableObject 추가
    - 맵 상호작용 인터페이스 추가

---
+++ JellyMario/Assets/Scripts/Map/GoalController.cs
@@ -0,0 +1,28 @@
+﻿using JellyMario.Player;
+using UnityEngine;
+
+namespace JellyMario.Map
+{
+    public class GoalController : MonoBehaviour
+    {
+        // 플레이어가 골인했을 때 호출
+        public virtual void ClearStage()
+        {
+            // TODO : 스테이지 클리어 처리
+            // 예)
+            // ManagersHub.Game.ClearStage();
+            // ManagersHub.Scene.LoadScene("NextStage");
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            // 플레이어인지 확인
+            PlayerBase player = collision.GetComponent<PlayerBase>();
+
+            if (player == null)
+                return;
+
+            ClearStage();
+        }
+    }
+}
