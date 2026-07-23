e605322cc5d8e80a6a2ab20b95dd82642777364d
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-07-24 05:07:28 +0900

    feat(client-framework): UI 구조 추가
    - UIBase 기본 클래스 추가
    - MainMenuUI 프레임워크 추가
    - InGameUI 프레임워크 추가
    - ResultUI 프레임워크 추가
    - RankingUI 프레임워크 추가

---
+++ JellyMario/Assets/Scripts/UI/RankingUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace JellyMario.UI
+{
+    public class RankingUI : UIBase
+    {
+        // 랭킹 표시
+        public void ShowRanking()
+        {
+        }
+
+        // 닫기
+        public void OnClickClose()
+        {
+        }
+    }
+}
