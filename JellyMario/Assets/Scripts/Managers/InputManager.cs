1b6462aead8c1b1816c7023f74fcf8ff03c33563
lsblsb0219 <146863475+lsblsb0219@users.noreply.github.com>
2026-07-23 18:42:57 +0900

    feat(client-framework): 입력 프레임워크 구현
    - InputManager 추가
    - 플레이어 입력 관리 구조 정의
    - 이동 입력 반환 함수 추가
    - 달리기 입력 반환 함수 추가
    - 점프 입력 반환 함수 추가
    - ManagersHub에 InputManager 등록

---
+++ JellyMario/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using JellyMario.Core;
+
+namespace JellyMario.Managers
+{
+    // 플레이어 입력을 관리하는 매니저
+    public class InputManager : Singleton<InputManager>
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        // 이동 입력 반환
+        public virtual Vector2 GetMoveInput()
+        {
+
+            return Vector2.zero;
+        }
+
+        // 달리기 입력 반환
+        public virtual bool GetRunInput()
+        {
+
+            return false;
+        }
+
+        // 점프 입력 반환
+        public virtual bool GetJumpInput()
+        {
+
+            return false;
+        }
+    }
+}
