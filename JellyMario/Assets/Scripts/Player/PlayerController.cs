ac4ab69b0407f54a355ee6adca66aaf4dc7c3b26
Seojin.K <7701seojin@gmail.com>
2026-07-30 22:15:49 +0900

    feat(client-movement): 플레이어 Jump 상태 추가
    - Jump, Hit, Die 상태 애니메이션 추가
    - Jump 상태 변화 구조 구현
    - Jump 상태 기본 설정 추가
    - Jump 함수 추가

--- JellyMario/Assets/Scripts/Player/PlayerController.cs
+++ JellyMario/Assets/Scripts/Player/PlayerController.cs
@@ -9,6 +9,11 @@ namespace JellyMario.Player
         [Header("Move 설정")]
         [SerializeField] private float moveSpeed = 5f;
 
+        [Header("Jump 설정")]
+        [SerializeField] private float jumpPower = 10f;
+        [SerializeField] private float jumpDuration = 0.5f;
+
+        private bool _isJumping;
         private Vector2 _moveInput;
 
         protected override void Initialize()
@@ -23,15 +28,25 @@ namespace JellyMario.Player
         {
             if (ManagersHub.Input == null) {
                 _moveInput = Vector2.zero;
+
                 return;
             }
 
             _moveInput = ManagersHub.Input.GetMoveInput();
+
+            if (!_isJumping && ManagersHub.Input.GetJumpInput())
+                _isJumping = true;
         }
 
         protected override void HandleMovement()
         {
-            if (Mathf.Abs(_moveInput.x) <= 0.01f) {
+            if (_isJumping)
+            {
+                Jump();
+                return;
+            }
+            else if (Mathf.Abs(_moveInput.x) <= 0.01f)
+            {
                 Idle();
                 return;
             }
@@ -45,6 +60,20 @@ namespace JellyMario.Player
 
             float distance = _moveInput.x * moveSpeed * Time.deltaTime;
             transform.position += Vector3.right * distance;
+        }
+
+        public override void Jump()
+        {
+            base.Jump();
+
+            if (transform.position.y < 0.1f)
+                transform.position += Vector3.up * jumpPower;
+            else
+            {
+                transform.position -= Vector3.up * jumpPower;
+
+                _isJumping = false;
+            }
         }
     }
 }
