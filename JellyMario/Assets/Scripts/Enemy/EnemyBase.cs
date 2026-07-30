261e7eb1233a9a312719521cd2423c382bc968e1
Seojin.K <7701seojin@gmail.com>
2026-07-30 21:26:34 +0900

    feat(client-enemy): 적 기본 구조 추가
    - EnemyBase.cs 추가
    - EnemyController.cs 추가

---
+++ JellyMario/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,10 @@
+using JellyMario.Player;
+using System.Collections;
+using UnityEngine;
+namespace JellyMario.Enemy
+{
+    // 모든 적의 부모 클래스
+    public class EnemyBase : MonoBehaviour
+    {
+    }
+}
---
+++ JellyMario/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace JellyMario.Enemy
+{
+    public class BasicEnemy : EnemyBase
+    {
+    }
+}
