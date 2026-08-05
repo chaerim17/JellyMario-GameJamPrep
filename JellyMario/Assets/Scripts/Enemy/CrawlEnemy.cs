51a90e08be8b72f0f00135fd069cea3759eb87b3
goqiewd <cheari1018@gmail.com>
2026-08-05 15:48:24 +0900

    feat(client-enemy): 기어다니는 적 구현 및 적 프리팹 구성
    - CrawlEnemy 이동 패턴 구현
    - 적 이동 상태 처리 로직 추가
    - SnailEnemy 오브젝트 생성
    - Circle Collider 2D 충돌 영역 설정
    - 적 애니메이션 프레임 연동 및 테스트

--- JellyMario/Assets/Scripts/Enemy/CrawlEnemy.cs
+++ JellyMario/Assets/Scripts/Enemy/CrawlEnemy.cs
@@ -1,16 +1,41 @@
+//달팽이
 using UnityEngine;
 
-public class CrawlEnemy : MonoBehaviour
+namespace JellyMario.Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CrawlEnemy : EnemyBase
     {
-        
-    }
+        [SerializeField] private float moveSpeed = 2f;
+        [SerializeField] private float moveRange = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Vector3 _startPosition;
+        private int _direction = 1;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _startPosition = transform.position;
+
+            Move();
+        }
+
+        protected override void HandleMovement()
+        {
+            transform.Translate(
+                Vector2.right * (_direction * moveSpeed * Time.deltaTime));
+
+            float distance =
+                transform.position.x - _startPosition.x;
+
+            if (Mathf.Abs(distance) >= moveRange)
+            {
+                _direction *= -1;
+
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+        }
     }
 }
