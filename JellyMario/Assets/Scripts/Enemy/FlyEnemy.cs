2fe36540b2d9d36b6c75f6d0556197c2a5a643ae
goqiewd <cheari1018@gmail.com>
2026-08-07 02:21:17 +0900

    feat(client-enemy): Bee 로직 수정
    - 기존 왕복 움직임에서 한 방향 움직임으로 변경
    - inspector로 1일 때 왼쪽, -1일 때 오른쪽 방향으로 이동됨

--- JellyMario/Assets/Scripts/Enemy/FlyEnemy.cs
+++ JellyMario/Assets/Scripts/Enemy/FlyEnemy.cs
@@ -5,26 +5,32 @@ namespace JellyMario.Enemy
 {
     public class FlyEnemy : EnemyBase
     {
-        [SerializeField] private float moveRange = 3f;
         [SerializeField] private float moveSpeed = 2f;
 
-        private Vector3 _startPosition;
+        // 1 = 왼쪽, -1 = 오른쪽
+        [SerializeField] private int direction = 1;
+
+        private SpriteRenderer _spriteRenderer;
 
         protected override void Awake()
         {
             base.Awake();
 
-            _startPosition = transform.position;
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+            if (_spriteRenderer != null)
+            {
+                _spriteRenderer.flipX = direction == -1;
+            }
 
             Move();
         }
 
         protected override void HandleMovement()
         {
-            float x = Mathf.Sin(Time.time * moveSpeed) * moveRange;
-
-            transform.position =
-                _startPosition + Vector3.right * x;
+            transform.Translate(
+                Vector2.left *
+                (direction * moveSpeed * Time.deltaTime));
         }
     }
 }
